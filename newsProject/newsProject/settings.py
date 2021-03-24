fba506e3a4842bdf7ef35c193d116e3022f7cbd6
sohaibmansuri <81158711+sohaibmansuri@users.noreply.github.com>
2021-03-24 10:41:02 +0530

    Update settings.py

--- newsProject/newsProject/settings.py
+++ newsProject/newsProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-%&5k6%23l16kw4l01v#&=_h&sen87zw2)*0_shxfm2%-sxfe8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sohaibmansuri.pythonanywhere.com']
 
 
 # Application definition
